966398e48be23b2338cfee2f716b290e214b1b88
DanXuuuuu <139434572+DanXuuuuu@users.noreply.github.com>
2026-02-07 23:53:24 -0800

    feat(hr): add onboarding APIs and token history endpoints

--- backend/routes/hrRoutes.js
+++ backend/routes/hrRoutes.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const hrController = require('../controllers/hrController');
 
-// get token history: register a route when people visit path, the func will be called
+// token history: register a route when people visit path, the func will be called
 router.get('/token-history', hrController.getTokenHistory);
-
 
 // onboarding applicaitons 
 router.get('/onboarding-applications', hrController.getOnboardingApplications);
@@ -14,29 +13,6 @@ router.patch('/onboarding/:id/approve', hrController.approveOnboarding);
 router.patch('/onboarding/:id/reject', hrController.rejectOnboarding);
 
 
-// get all employees
-router.get('/employees', hrController.getAllEmployees);
-
-
-// search employee 
-router.get('/employees/search', hrController.searchEmployees);
-
-
-// get one specific employee 
-router.get('/employees/:id', hrController.getEmployeeDetail);
-
-// get visa in progress 
-router.get('/visa/in-progress', hrController.getVisaInProgress);
-
-// get all employees who need Visa 
-router.get('/visa/all', hrController.getAllVisaEmployees);
-
-// approve/reject visa
-router.patch('/visa/:userId/:docType/approve', hrController.approveVisaDoc);
-router.patch('/visa/:userId/:docType/reject', hrController.rejectVisaDoc);
-
-// send visa reminder reminder email 
-router.post('/visa/:userId/send-reminder', hrController.sendVisaReminder);
 
 module.exports = router;
 
