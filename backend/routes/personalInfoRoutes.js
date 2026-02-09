0516d4f30f4e36db5c6a76c094a9549df7ad3dc6
Lian <lian1c@alma.edu>
2026-02-09 15:21:12 -0800

    update personal info routes

--- backend/routes/personalInfoRoutes.js
+++ backend/routes/personalInfoRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { protect, restrictTo } = require('../middleware/authMiddleware');
+const { protect} = require('../middleware/authMiddleware');
 
 const { getPersonalInfo, updatePersonalInfoSection } = require("../controllers/personalInfoController");
 
