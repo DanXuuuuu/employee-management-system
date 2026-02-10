68e01ef70112d5e5a4fb973851a1dcefbfd81b8f
Lian <lian1c@alma.edu>
2026-02-09 16:40:21 -0800

    add documents slice with fetch/upload/reupload

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from "./authSlice";
 import onboardingSliceReducer from "./onboardingSlice";
 import personalInfoSliceReducer from "./personalInfoSlice";
+import documentsReducer from "./documentsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSliceReducer,
     onboarding: onboardingSliceReducer,
     personalInfo: personalInfoSliceReducer,
+    documents: documentsReducer,
   },
 });
